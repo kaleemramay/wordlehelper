9b36522950bd5a683044a2e58ad9ca5d438bbf6c
kaleemramay <kaleemramay104@gmail.com>
2022-03-14 15:04:03 -0400

    fixed greens

--- app.py
+++ app.py
@@ -4,31 +4,31 @@ words = f.read()
 
 words_list = words.split("\n")
 
-exclude = ["w", "a", "t", "e", "r", "h", "l", "p", "k", "i", "n", "d"]
+exclude = list("frakon")
+#exclude = ["w", "a", "t", "e", "r", "h", "l", "p", "k", "i", "n", "d"]
 
 greens = {
     '0': "",
-    '1': "",
-    '2': "",
+    '1': "m",
+    '2': "e",
     '3': "",
-    '4': "s"
+    '4': ""
 }
 
 pruned_list = []
 positional_list = []
 
-#check words list and remove words that contain exclude and save to pruned_list
 for word in words_list:
     if not any(char in exclude for char in word):
         pruned_list.append(word)
 
-for i in greens:
-    for word in pruned_list:
-        #print(list(word)[int(i)])
-        # print(word)
-        # print(list(word)[int(i)])
-        
-        if list(word)[int(i)] == greens[i]:
-            positional_list.append(word)
+for word in pruned_list:
+    addWord = True 
+    for i in greens:
+        if greens[i] != "":
+            if  not list(word)[int(i)] == greens[i]:
+                addWord = False
+    if addWord == True:
+        positional_list.append(word)
 
 print(positional_list)
