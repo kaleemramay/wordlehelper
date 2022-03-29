a572658e3218621ed8baaa4500f28daac163ae5d
kaleemramay <kaleemramay104@gmail.com>
2022-03-29 02:38:15 -0400

    Added functionality for yellow letters

--- app.py
+++ app.py
@@ -4,25 +4,40 @@ words = f.read()
 
 words_list = words.split("\n")
 
-exclude = list("frakon")
+exclude = list("wetcrfm")
 #exclude = ["w", "a", "t", "e", "r", "h", "l", "p", "k", "i", "n", "d"]
 
+yellows = list("")
+
 greens = {
-    '0': "",
-    '1': "m",
-    '2': "e",
-    '3': "",
-    '4': ""
+    '0': "s",
+    '1': "",
+    '2': "a",
+    '3': "l",
+    '4': "l"
 }
 
 pruned_list = []
+included_list = []
 positional_list = []
 
 for word in words_list:
     if not any(char in exclude for char in word):
         pruned_list.append(word)
 
+# a loop to iterate through pruned_list
 for word in pruned_list:
+    include = True
+    for char in yellows:
+        if char not in word:
+            include = False
+        else:
+            print(word)
+    if include:
+        included_list.append(word)
+# print(included_list)
+
+for word in included_list:
     addWord = True 
     for i in greens:
         if greens[i] != "":
